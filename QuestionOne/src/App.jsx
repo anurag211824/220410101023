7d0340f97f9d1dc3a0b1ef6ded12da7720ace324
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2025-03-21 07:30:30 +0530

    first Commit

---
+++ QuestionOne/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-black'>
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App
