b3bd0f56227572613ba86a5f7698a3d8e37dd2ca
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2025-03-21 11:59:51 +0530

    sir api is not working so i demonstrated a basic product api

---
+++ QuestionOne/src/pages/About.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About this SocialMedia</h2>
+      <p>
+      
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, cum!
+      </p>
+    </div>
+  );
+};
+
+export default About;
